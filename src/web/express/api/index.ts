17f89ab510fc6cf151b772423febda268466a723
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2021-06-30 16:22:29 +0900

    get userinfo

--- src/web/express/api/index.ts
+++ src/web/express/api/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { Request, Response, Router } from 'express'
 import jwtAuthz from 'express-jwt-authz'
 // import memberRoutes from './member.api'
@@ -8,6 +9,15 @@ const router = Router()
 const checkScopes = jwtAuthz([ 'email' ])
 
 router.get('/authorized', checkScopes, function (req: Request, res: Response) {
+  const result = axios.get('https://s2factory.jp.auth0.com/userinfo', {
+    headers: {
+      Authorization: req.header('Authorization') ?? '',
+    },
+  })
+    .then(function (res) {
+      console.log(res.data)
+    })
+
   const user = (req as unknown as { user: { sub: string } }).user
   console.log(user)
   res.send('有効なセッションです')
